b2d8588d0eec2a2d8799c9c877e3e983295b5493
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <132590839+Harry2876@users.noreply.github.com>
2023-12-06 16:10:14 +0530

    Revert "Update Client"
    This reverts commit 3d6e4d5a42c74e573490a5a0475d2e66625142fc.

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: "d87bcc39-1750-4ca0-ad54-f8d0efbb2735",
-  obfuscatedClientSecret: "U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==",
+  clientId: "eb3e2b0d-5ed9-417c-8e72-19a4a36e0a94",
+  obfuscatedClientSecret: "U2FsdGVkX18eYVMPEQcBVlIhdq1P8ZaDlkBYURPHYwZ+HlLczNksjNf7wO6TxpKLR5RFMmThRumFn7mXzjjPhw==",
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
