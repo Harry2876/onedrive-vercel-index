92726e82d0fe37b0cc7618d21ddb1aa4f5b2f6c8
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <132590839+Harry2876@users.noreply.github.com>
2024-03-04 19:57:42 +0530

    Locked js folder

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Mastering Java SE programming from Beginner to Master', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/Mastering Java SE programming from Beginner to Master','/JavaScript', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "acbcharryxyz@gmail.com",
