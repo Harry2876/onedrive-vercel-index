da3fcf623778f0106f219a7b055332671379ea49
Hariom Harsh <132590839+Harry2876@users.noreply.github.com>
2025-02-15 08:05:02 +0000

    Adjustments

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -11,10 +11,17 @@ const Loading: React.FC<{ loadingText: string }> = ({ loadingText }) => {
 export const LoadingIcon: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+       <defs>
+        <linearGradient id="loadingGradient" x1="100%" y1="0%" x2="0%" y2="0%">
+          <stop offset="0%" stopColor="#ff0000" />
+          <stop offset="50%" stopColor="#ff9900" />
+          <stop offset="100%" stopColor="#ff0000" />
+        </linearGradient>
+      </defs>
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="url(#loadingGradient)" strokeWidth="4" />
       <path
         className="opacity-75"
-        fill="currentColor"
+        fill="url(#loadingGradient)"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
     </svg>
